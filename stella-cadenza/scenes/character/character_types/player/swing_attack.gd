252eaab021cb4a262d3e04241dce873411d7f446
Contemplationjpg <kaithekidrs2004@gmail.com>
2025-07-11 15:17:43 -0700

    swing and tempo
    swing attack now only swings if button is being pressed on beat
    tempo is now based on position on the audio_stream instead of elapsed time, as to account for potential delays and lag

--- stella-cadenza/scenes/character/character_types/player/swing_attack.gd
+++ stella-cadenza/scenes/character/character_types/player/swing_attack.gd
@@ -10,7 +10,8 @@ extends Node2D
 @export var attack_slow : float = 0.5
 var can_attack : bool = false
 var attacking : bool = false
-var original_velocity : float
+var should_be_attacking : bool = false
+var original_velocity : float 
 
 signal StartAttack
 signal StopAttack
@@ -18,47 +19,73 @@ signal StopAttack
 func _ready() -> void:
 	sprite.visible = false
 	can_attack = true
+	# original_velocity = chara.max_velocity
 	SignalBus.EvenBeat.connect(got_even_beat)
 	SignalBus.OddBeat.connect(got_odd_beat)
 
 func got_odd_beat():
-	print("odd")
+	# print("odd")
+	# attack()
+	return
 
 func got_even_beat():
-	print("even")
+	# print("even")
+	attack()
+
+func attack():
+	if should_be_attacking:
+		StartAttack.emit()
+		# can_attack = false
+		sprite.visible = true
+		swing_hitbox.change_active(true)
+		await get_tree().create_timer(active_time).timeout
+		swing_hitbox.change_active(false)
+		sprite.visible = false
+		StopAttack.emit()
+
+
 
 
 func _process(_delta: float) -> void:
 	if Main.paused:
 		return
 	# print(timer.time_left)
-	if can_attack:
+
+	if can_attack and Input.is_action_pressed("attack"):
+		should_be_attacking = true
+		# print("sba is true")
+	else:
+		should_be_attacking = false
+		# print("sba is false")
+
+
+	# if can_attack:
 		# print("can attack")
-		if Input.is_action_just_pressed("attack") and not attacking and timer.time_left <=0.0:
-			# print("attacking")
-			original_velocity = chara.max_velocity
-			chara.max_velocity = original_velocity*attack_slow
-			attacking = true
-			state_machine.force_change_state("Swinging")
-			StartAttack.emit()
-			can_attack = false
-			sprite.visible = true
-			swing_hitbox.change_active(true)
-			await get_tree().create_timer(active_time).timeout
-			swing_hitbox.change_active(false)
-			sprite.visible = false
-			while Input.is_action_pressed("attack"):
-				StartAttack.emit()
-				sprite.visible = true
-				swing_hitbox.change_active(true)
-				timer.start()
-				await get_tree().create_timer(active_time).timeout
-				swing_hitbox.change_active(false)
-				sprite.visible = false
-			attacking = false
-			timer.start()
-			chara.max_velocity = original_velocity
-			StopAttack.emit()
+		# if Input.is_action_just_pressed("attack") and not attacking and timer.time_left <=0.0:
+		# 	# print("attacking")
+		# 	original_velocity = chara.max_velocity
+		# 	chara.max_velocity = original_velocity*attack_slow
+		# 	attacking = true
+		# 	state_machine.force_change_state("Swinging")
+		# 	StartAttack.emit()
+		# 	can_attack = false
+		# 	sprite.visible = true
+		# 	swing_hitbox.change_active(true)
+		# 	await get_tree().create_timer(active_time).timeout
+		# 	swing_hitbox.change_active(false)
+		# 	sprite.visible = false
+		# 	while Input.is_action_pressed("attack"):
+		# 		StartAttack.emit()
+		# 		sprite.visible = true
+		# 		swing_hitbox.change_active(true)
+		# 		timer.start()
+		# 		await get_tree().create_timer(active_time).timeout
+		# 		swing_hitbox.change_active(false)
+		# 		sprite.visible = false
+		# 	attacking = false
+		# 	timer.start()
+		# 	chara.max_velocity = original_velocity
+		# 	StopAttack.emit() 
 
 func _on_attack_cooldown_timeout() -> void:
 	can_attack = true
--- stella-cadenza/scenes/levels/tempo.gd
+++ stella-cadenza/scenes/levels/tempo.gd
@@ -1,30 +1,82 @@
 class_name Tempo
 extends Timer
 
-@export var bpm : int = 100
+var bpm : float = 100
 @export var debug : TextureRect
+@export var audio_stream : AudioStreamPlayer2D
 var beat_duration : float
 var beat_count : int = 0 #this is set up assuming all songs will be 4/4
+var time_begin : float
+var time_delay : float
+var flashing : bool = false
+
 
 func _ready():
-	beat_duration = 60.0/bpm
-	wait_time = beat_duration
-
-
-
-func _on_timeout() -> void:
-	beat_count+=1
-	if debug:
-		if beat_count%2 == 0:
-			SignalBus.EvenBeat.emit()
-		else:
-			SignalBus.OddBeat.emit()
-		if beat_count%4==0:
-			debug.self_modulate.s = 1
+	set_bpm(audio_stream.stream.bpm)
+	# wait_time = beat_duration
+	# start()
+	audio_stream.play()
+	
+func _process(_delta: float) -> void:
+	var time : float = audio_stream.get_playback_position() + AudioServer.get_time_since_last_mix()
+	time -= AudioServer.get_output_latency()
+	# print("Time is: ", time)
+	var mod = find_mod(time, beat_duration)
+	# print(mod)
+	if not flashing:
+		if mod < 0.02 and time > 0:
+			beat_count += 1
+			# print("gaming ", time, " ", beat_count)
+			flashing = true 
+			if beat_count%2 == 0:
+				debug.self_modulate.s = 1
+				SignalBus.EvenBeat.emit()
+			else:
+				SignalBus.OddBeat.emit()
 			await get_tree().create_timer(beat_duration/2).timeout
 			debug.self_modulate.s = 0
-		else:
-			debug.self_modulate.s = .25
-			await get_tree().create_timer(beat_duration/2).timeout
-			debug.self_modulate.s = 0
+			flashing = false
+
+func set_bpm(new_bpm : float):
+	bpm = new_bpm
+	print(bpm)
+	beat_duration = 60.0/bpm 
+
+func find_mod(a : float, b : float):
+	if a < 0:
+		a = -a
+	if b < 0:
+		b = -b
+	var mod : float = a
+	while mod >= b:
+		mod = mod - b
+	if a < 0:
+		return -mod
+	return mod
+
+# func _on_timeout() -> void:
+# 	beat_count+=1
+# 	if debug:
+# 		if beat_count%2 == 0:
+# 			SignalBus.EvenBeat.emit()
+# 		else:
+# 			SignalBus.OddBeat.emit()
+# 		if beat_count%4==0:
+# 			debug.self_modulate.s = 1
+# 			await get_tree().create_timer(beat_duration/2).timeout
+# 			debug.self_modulate.s = 0
+# 		else:
+# 			debug.self_modulate.s = .25
+# 			await get_tree().create_timer(beat_duration/2).timeout
+# 			debug.self_modulate.s = 0
 			
+
+
+# func _on_audio_stream_player_2d_finished() -> void:
+# 	print("AUDIO STOPPED!!!!!")
+# 	wait_time = beat_duration
+# 	beat_count=0
+# 	start()
+# 	audio_stream.play()
+# 	# audio_stream.stream_paused = false
+# 	# audio_stream.play()
