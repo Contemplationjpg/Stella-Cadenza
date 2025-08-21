f690e818b372e046701d39d69106d142ca7464f7
Contemplationjpg <kaithekidrs2004@gmail.com>
2025-08-20 20:18:44 -0700

    added camera follower "lakitu"
    camera anchored on player but follows mouse up to specified max distance, applied 1.5* for y axis

---
+++ stella-cadenza/scenes/main/lakitu.gd
@@ -0,0 +1,42 @@
+#holds the camera and follows the player but drifts towards the mouse
+extends Node2D
+
+@export var camera : Camera2D
+@export var player : Player
+@export var min_distance : float = 50
+@export var max_distance : float = 200
+@export var camera_speed : float = 0.25
+
+var anchor : Vector2
+var mouse : Vector2
+var target_position : Vector2
+
+
+func _process(delta):
+	anchor = player.global_position
+	mouse = get_local_mouse_position()
+	# print(anchor.x-mouse.x)
+	# print(mouse.x)
+
+	if abs(mouse.x) > max_distance:
+		target_position.x = anchor.x + (sign(mouse.x) * max_distance)
+		# print("XMAX")
+	elif abs(mouse.x) > min_distance:
+		target_position.x = anchor.x + mouse.x
+	else:
+		target_position.x = anchor.x
+
+	if abs(mouse.y) > max_distance/1.5:
+		target_position.y = anchor.y + (sign(mouse.y)*max_distance)/1.5
+		# print("YMAX")
+	elif abs(mouse.y) > min_distance/1.5:
+		target_position.y = anchor.y + mouse.y/1.5
+	else:
+		target_position.y = anchor.y
+
+	# print(target_position)
+	var cam_move_speed = camera_speed * delta
+	if abs(mouse.x) and abs(mouse.y) > max_distance:
+		cam_move_speed *= 1.4
+	camera.global_position = lerp(anchor,target_position,cam_move_speed)
+	
