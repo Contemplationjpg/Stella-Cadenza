7cfab2926886bbde2040741e0937ffecacbf903d
Contemplationjpg <kaithekidrs2004@gmail.com>
2025-08-22 03:49:29 -0700

    "c_" (current) versions of stop_velocity, max_velocity, and acceleration
    done so that stats can be modulated and eventually returned to normal, more may be added later

--- stella-cadenza/scenes/character/character.gd
+++ stella-cadenza/scenes/character/character.gd
@@ -16,6 +16,9 @@ var facing : int
 enum directions {up, right, down, left}
 var direction: Vector2
 @onready var current_health = base_stats.max_health
+@onready var c_stop_velocity = stop_velocity
+@onready var c_max_velocity = max_velocity
+@onready var c_acceleration = acceleration
 
 func _ready() -> void:
 	debug.text = "health: " + str(current_health)
@@ -71,48 +74,49 @@ func update_movement(delta : float):
 				velocity.y = -velocity.y
 			if direction.x != 0 and direction.y != 0:
 				# print("xydir")
-				if (velocity.x >= 0 and velocity.x < max_velocity*.71) or (velocity.x < 0 and velocity.x > -max_velocity*.71):
+				if (velocity.x >= 0 and velocity.x < c_max_velocity*.71) or (velocity.x < 0 and velocity.x > -c_max_velocity*.71):
 					if (velocity.x >= 0 and velocity.x < base_velocity*.71) or (velocity.x < 0 and velocity.x > -base_velocity*.71):
 						velocity.x = direction.x*base_velocity*.71
-					velocity.x += direction.x*acceleration*delta
-				if (velocity.y >= 0 and velocity.y < max_velocity*.71) or (velocity.y < 0 and velocity.y > -max_velocity*.71):
+					velocity.x += direction.x*c_acceleration*delta
+				if (velocity.y >= 0 and velocity.y < c_max_velocity*.71) or (velocity.y < 0 and velocity.y > -c_max_velocity*.71):
 					if (velocity.y >= 0 and velocity.y < base_velocity*.71) or (velocity.y < 0 and velocity.y > -base_velocity*.71):
 						velocity.y = direction.y*base_velocity*.71
-					velocity.y += direction.y*acceleration*delta
-				if (velocity.x >= 0 and velocity.x > max_velocity*.71) or (velocity.x < 0 and velocity.x < -max_velocity*.71):
-					velocity = velocity.move_toward(Vector2(max_velocity*.71 * sign(velocity.x), velocity.y), stop_velocity)
-				if (velocity.y >= 0 and velocity.y > max_velocity*.71) or (velocity.y < 0 and velocity.y < -max_velocity*.71):
-					velocity = velocity.move_toward(Vector2(velocity.x, max_velocity*.71 * sign(velocity.y)), stop_velocity)
+					velocity.y += direction.y*c_acceleration*delta
+				if (velocity.x >= 0 and velocity.x > c_max_velocity*.71) or (velocity.x < 0 and velocity.x < -c_max_velocity*.71):
+					velocity = velocity.move_toward(Vector2(max_velocity*.71 * sign(velocity.x), velocity.y), c_stop_velocity)
+				if (velocity.y >= 0 and velocity.y > c_max_velocity*.71) or (velocity.y < 0 and velocity.y < -c_max_velocity*.71):
+					velocity = velocity.move_toward(Vector2(velocity.x, c_max_velocity*.71 * sign(velocity.y)), c_stop_velocity)
 			elif direction.x != 0:
 				# print("xdir")
 				# if (direction.x > 0 and velocity.x < 0) or (direction.x < 0 and velocity.x > 0):
 				# 	velocity.x = -velocity.x
-				if (velocity.x >= 0 and velocity.x < max_velocity) or (velocity.x < 0 and velocity.x > -max_velocity):
+				if (velocity.x >= 0 and velocity.x < c_max_velocity) or (velocity.x < 0 and velocity.x > -c_max_velocity):
 					if (velocity.x >= 0 and velocity.x < base_velocity) or (velocity.x < 0 and velocity.x > -base_velocity):
 						velocity.x = direction.x*base_velocity
-					velocity.x += direction.x*acceleration*delta
-				if (velocity.x >= 0 and velocity.x > max_velocity) or (velocity.x < 0 and velocity.x < -max_velocity):
-					velocity = velocity.move_toward(Vector2(max_velocity * sign(velocity.x), 0), stop_velocity)
+					velocity.x += direction.x*c_acceleration*delta
+				if (velocity.x >= 0 and velocity.x > c_max_velocity) or (velocity.x < 0 and velocity.x < -c_max_velocity):
+					velocity = velocity.move_toward(Vector2(max_velocity * sign(velocity.x), 0), c_stop_velocity)
 						
 			elif direction.y != 0:
 				# print("ydir")
 				# if (direction.y > 0 and velocity.y < 0) or (direction.y < 0 and velocity.y > 0):
 				# 	velocity.y = -velocity.y
-				if (velocity.y >= 0 and velocity.y < max_velocity) or (velocity.y < 0 and velocity.y > -max_velocity):
+				if (velocity.y >= 0 and velocity.y < c_max_velocity) or (velocity.y < 0 and velocity.y > -c_max_velocity):
 					if (velocity.y >= 0 and velocity.y < base_velocity) or (velocity.y < 0 and velocity.y > -base_velocity):
 						velocity.y = direction.y*base_velocity
-					velocity.y += direction.y*acceleration*delta
-				if (velocity.y >= 0 and velocity.y > max_velocity) or (velocity.y < 0 and velocity.y < -max_velocity):
-					velocity = velocity.move_toward(Vector2(0, max_velocity * sign(velocity.y)), stop_velocity)
+					velocity.y += direction.y*c_acceleration*delta
+				if (velocity.y >= 0 and velocity.y > c_max_velocity) or (velocity.y < 0 and velocity.y < -c_max_velocity):
+					velocity = velocity.move_toward(Vector2(0, c_max_velocity * sign(velocity.y)), c_stop_velocity)
 			# print("slowing")
-			velocity = velocity.move_toward(Vector2.ZERO, stop_velocity)
+			velocity = velocity.move_toward(Vector2.ZERO, c_stop_velocity)
 
 		else:
 			# print("slowing fast")
-			velocity = velocity.move_toward(Vector2.ZERO, stop_velocity_still)
+			# velocity = velocity.move_toward(Vector2.ZERO, stop_velocity_still)
+			velocity = velocity.move_toward(Vector2.ZERO, c_stop_velocity)
 		# print(velocity)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, stop_velocity)
+		velocity = velocity.move_toward(Vector2.ZERO, c_stop_velocity)
 			
 
 
