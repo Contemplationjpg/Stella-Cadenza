de6016a203f5651673f111eeeab2c1cb9b0cb30f
Contemplationjpg <kaithekidrs2004@gmail.com>
2025-08-22 03:47:04 -0700

    changed Y bounds of camera to be on par with X bounds

--- stella-cadenza/scenes/main/lakitu.gd
+++ stella-cadenza/scenes/main/lakitu.gd
@@ -18,25 +18,43 @@ func _process(delta):
 	# print(anchor.x-mouse.x)
 	# print(mouse.x)
 
-	if abs(mouse.x) > max_distance:
+	if abs(mouse.x) >= max_distance:
 		target_position.x = anchor.x + (sign(mouse.x) * max_distance)
 		# print("XMAX")
-	elif abs(mouse.x) > min_distance:
+	elif abs(mouse.x) >= min_distance:
 		target_position.x = anchor.x + mouse.x
 	else:
 		target_position.x = anchor.x
-
-	if abs(mouse.y) > max_distance/1.5:
-		target_position.y = anchor.y + (sign(mouse.y)*max_distance)/1.5
+	
+	
+	if abs(mouse.y) >= max_distance:
+		target_position.y = anchor.y + (sign(mouse.y)*max_distance)
 		# print("YMAX")
-	elif abs(mouse.y) > min_distance/1.5:
-		target_position.y = anchor.y + mouse.y/1.5
+	elif abs(mouse.y) >= min_distance:
+		target_position.y = anchor.y + mouse.y
 	else:
 		target_position.y = anchor.y
 
+
+	# if abs(mouse.y) >= max_distance/1.5:
+	# 	target_position.y = anchor.y + (sign(mouse.y)*max_distance)/1.5
+	# 	# print("YMAX")
+	# elif abs(mouse.y) >= min_distance/1.5:
+	# 	target_position.y = anchor.y + mouse.y/1.5
+	# else:
+	# 	target_position.y = anchor.y
+
 	# print(target_position)
+
+
 	var cam_move_speed = camera_speed * delta
-	if abs(mouse.x) and abs(mouse.y) > max_distance:
-		cam_move_speed *= 1.4
-	camera.global_position = lerp(anchor,target_position,cam_move_speed)
+	if abs(mouse.x) >= max_distance and abs(mouse.y) >= max_distance:
+		cam_move_speed /= 1.4
+	# print(target_position)
+	# print(camera.global_position)
+	# if camera.global_position == target_position:
+	# 	return
+
+	camera.global_position.x = lerp(anchor.x,target_position.x,cam_move_speed)
+	camera.global_position.y = lerp(anchor.y,target_position.y,cam_move_speed)
 	
