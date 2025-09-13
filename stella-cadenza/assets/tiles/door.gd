bd08702473569712c1ff75121564edd079f8e2a8
M. Saechao <55562924+Contemplationjpg@users.noreply.github.com>
2025-09-12 17:11:11 -0700

    door now has blocker that stops player from entering door until screen unblock ends

--- stella-cadenza/assets/tiles/door.gd
+++ stella-cadenza/assets/tiles/door.gd
@@ -3,20 +3,49 @@ extends Area2D
 
 @export var level : String = "none"
 @export var active : bool = true
+@export var blocker: CollisionShape2D
 @export var specific_exit : bool = false
 @export var exit_coords : Vector2
+@export var wait_for_specific_dialogue : bool = false
+@export var dialogue : Dialogue_Interact
+
+
+var dialogue_interacted_with : bool = false
+
+func _ready():
+	blocker.set_deferred("disabled", false)
+	SignalBus.JustUnblocked.connect(on_unblock)
+	if wait_for_specific_dialogue and dialogue:
+		dialogue.DialogueOver.connect(just_interacted_with_dialogue)
+
+
+func just_interacted_with_dialogue():
+	dialogue_interacted_with = true
+
+func on_unblock():
+	print("just unblocked")
+	blocker.set_deferred("disabled", true)
+
+
+
+
 
 func _on_body_entered(body:Node2D) -> void:
 	if active:
+		if wait_for_specific_dialogue and not dialogue_interacted_with:
+			print("dia not finished")
+			return
 		var player : Player = body as Player
-		# print("something at door")
+		print("something at door")
 		if player:
-			# print("DOOR FOUND PLAYER")
+			active = false
+			print("DOOR FOUND PLAYER")
 			if not player.can_walk_through_door:
-				# print("PLAYER CANNOT WALK THROUGH DOOR")
+				print("PLAYER CANNOT WALK THROUGH DOOR")
+				active = true
 				return
+			print("player going through!")
 			if level != "none":
-				active = false
 				if specific_exit:
 					SignalBus.LoadLevelAtLocation.emit(level, exit_coords)
 					return
